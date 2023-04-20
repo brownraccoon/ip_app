ad4312f18d0b66dd8e090a6774529fb749c9dac1
brownraccoon <brownraccoonstudios@gmail.com>
2023-04-20 20:06:34 +0530

    added api interface

--- app/src/main/java/com/br/ipapp/di/AppDI.kt
+++ app/src/main/java/com/br/ipapp/di/AppDI.kt
@@ -3,6 +3,7 @@ package com.br.ipapp.di
 import android.content.Context
 import com.br.ipapp.BuildConfig
 import com.br.ipapp.network.AuthInterceptor
+import com.br.ipapp.network.IpApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,6 +11,8 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -30,12 +33,30 @@ object AppDI {
             httpBuilder
                 .readTimeout(60, TimeUnit.SECONDS)
                 .writeTimeout(60, TimeUnit.SECONDS)
-                .connectTimeout(60, TimeUnit.SECONDS)
-                .addInterceptor(loggingInterceptor)
+                .connectTimeout(60, TimeUnit.SECONDS).addInterceptor(loggingInterceptor)
                 .build()
         } else {
             httpBuilder.build()
         }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val baseApiUrl = "https://ipapi.co/"
+        return Retrofit.Builder()
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .baseUrl(baseApiUrl)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideIpApi(retrofit: Retrofit): IpApi {
+        return retrofit.create(IpApi::class.java)
     }
 
 }
---
+++ app/src/main/java/com/br/ipapp/network/IpApi.kt
@@ -0,0 +1,11 @@
+package com.br.ipapp.network
+
+import com.br.ipapp.networkmodel.IpResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface IpApi {
+    @GET
+    suspend fun getClientIpInfo(
+    ): Response<IpResponse>
+}
